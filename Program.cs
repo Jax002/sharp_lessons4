1824eb9b8479729e761fd373e6d3cb1deb642779
Евгений <@DESKTOP-58GCN60>
2026-03-13 22:36:44 +0200

    done homework

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿// 1 Створення об'єкта
+using sharp_lessons4;
+
+Warehouse warehouse = new Warehouse();
+
+// 2 Підписка на подію
+warehouse.OnItemChanged += (index, item) =>
+{
+    Console.WriteLine($"Лог: У комірку №{index} додано товар: {item}");
+};
+
+// 3 Фільтрація (Predicate):
+Predicate<string> isNotEmpty = product => !string.IsNullOrWhiteSpace(product);
+
+// 4 Обробка даних (Func)
+Func<string, string> toUpper = product => product?.ToUpper() ?? string.Empty;
+
+// Додавання товарів на склад
+string[] products = { "Ноутбук", "", "Планшет", null, "ПК", "  ", "Монитор" };
+
+for (int i = 0; i < products.Length && i < warehouse.Size; i++)
+{
+    if (isNotEmpty(products[i]))
+    {
+        warehouse[i] = toUpper(products[i]);
+    }
+}
+    
---
+++ Warehouse.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sharp_lessons4
+{
+    public class Warehouse
+    {
+        private string[] _items;
+
+        public event Action<int, string> OnItemChanged;
+
+        public Warehouse()
+        {
+            _items = new string[10];
+        }
+
+        public string this[int index]
+        {
+            get
+            {
+                return _items[index];
+            }
+            set
+            {
+                _items[index] = value;
+                OnItemChanged?.Invoke(index, value);
+            }
+        }
+
+        public int Size => _items.Length;
+    }
+}
